941b8452201724345a6cb19a9043801a7fbc20d7
Victoria <Victory@n1866.BTI.DOMAIN>
2024-03-21 12:29:40 +0300

    fix from fix branch

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 def lazy_function(a, b):
+    "blablabla"
+
     print('hello BOYS')
     print(a, b)
+
+
+def new():
+    pass
 
 
 def my_feature_function():
     """makes some things"""
 
-    a = 12
-    b = 19
-
-    return a + b 
+    size = 44
+    price = 19
